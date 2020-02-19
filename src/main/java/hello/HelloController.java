1aefdbddee077fd7aec48f5c9d96a4af1b46f845
0111sandesh <0111sandesh@users.noreply.github.com>
2020-02-18 22:38:31 -0500

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -32,7 +32,7 @@ public class HelloController {
 
         logger.fatal("This is a fatal statement");
 
-        return "Welcome to this DEMO 2!"; 
+        return "Welcome to this DEMO 3!"; 
 
     } 
 
